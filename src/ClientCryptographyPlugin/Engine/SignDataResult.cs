c94091b62ab302f438d3c0d2e2b78f6c8fdeb200
Мосолов Константин (Mosolov_KN) <Mosolov_KN@directum.ru>
2023-04-21 14:35:29 +0400

    Новое API клиентского плагина

---
+++ src/ClientCryptographyPlugin/Engine/SignDataResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Library
+{
+  /// <summary>
+  /// Результат выполнения операции подписания.
+  /// </summary>
+  public enum SignDataResult
+  {
+    Success = 0,
+    NeedFileName = 1,
+    InvalidPassword = 2,
+    UnknownError = 3,
+    IllegalCertificateID = 4,
+    NotFound = 5,
+    InvalidFile = 6
+  }
+}
