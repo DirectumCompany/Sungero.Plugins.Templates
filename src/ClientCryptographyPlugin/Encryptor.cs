3693a911dce50fcd53f2d4a3103befb21717fd52
Иванов Денис (Ivanov_DI) <Ivanov_DI@directum.ru>
2025-04-14 14:39:47 +0400

    Merged PR 47479: Шифрование клиентскими сертификатами. Новый шаблон клиентского плагина
    Добавлен Encryptor с методами шифрования и расшифрования
    
    Related work items: #366310

---
+++ src/ClientCryptographyPlugin/Encryptor.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Library
+{
+    /// <summary>
+    /// Класс клиентского шифрования.
+    /// </summary>
+    public static class Encryptor
+    {
+        /// <summary>
+        /// Количество итераций при получении ключа симметричного шифрования.
+        /// </summary>
+        private const int KeyDeriveIterations = 64;
+
+        /// <summary>
+        /// Количество бит в байте.
+        /// </summary>
+        private const int BitsPerByte = 8;
+
+        /// <summary>
+        /// Вектор инициализации.
+        /// </summary>
+        private static byte[] IV = Convert.FromBase64String("Pg8FDx9EUNhFPPbq1FGbcA==");
+
+        /// <summary>
+        /// Материал для ключа.
+        /// </summary>
+        private static byte[] keyMaterial = Encoding.ASCII.GetBytes("SuperSecret");
+
+        /// <summary>
+        /// Материал для нового ключа.
+        /// </summary>
+        private static byte[] newKeyMaterial = Encoding.ASCII.GetBytes("TopSecret");
+
+        /// <summary>
+        /// Метод шифрования данных.
+        /// </summary>
+        /// <param name="decryptedStream">Поток незашифрованных данных (исходные данные).</param>
+        /// <param name="encryptedStream">Поток шифрованных данных (результат).</param>
+        /// <param name="encryptOptions">Параметры шифрования переданные с сервера.</param>
+        /// <param name="errorMessage">Сообщение об ошибке (если она есть).</param>
+        /// <returns>Результат выполнения операции (см. <see cref="SignDataResult"/>).</returns>
+        public static int Encrypt(Stream decryptedStream, Stream encryptedStream, string encryptOptions, out string errorMessage)
+        {
+            Logger.Debug($"Encrypt encryptOptions: {encryptOptions}");
+            errorMessage = string.Empty;
+
+            using (var keyGenerator = new Rfc2898DeriveBytes(keyMaterial, IV, KeyDeriveIterations))
+            using (var aes = Aes.Create())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.IV = IV;
+                aes.Key = keyGenerator.GetBytes(aes.KeySize / BitsPerByte);
+                using (var cryptoStream = new CryptoStream(encryptedStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    decryptedStream.CopyTo(cryptoStream);
+                    cryptoStream.FlushFinalBlock();
+                }
+            }
+
+            return (int)SignDataResult.Success;
+        }
+
+        /// <summary>
+        /// Метод расшифровки данных.
+        /// </summary>
+        /// <param name="encryptedStream">Поток шифрованных данных (исходные данные).</param>
+        /// <param name="decryptedStream">Поток незашифрованных данных (результат).</param> 
+        /// <param name="decryptOptions">Параметры шифрования переданные с сервера.</param>
+        /// <param name="errorMessage">Сообщение об ошибке (если она есть).</param>
+        /// <returns>Результат выполнения операции (см. <see cref="SignDataResult"/>).</returns>
+        public static int Decrypt(Stream encryptedStream, Stream decryptedStream, string decryptOptions, out string errorMessage)
+        {
+            Logger.Debug($"Decrypt decryptOptions: {decryptOptions}");
+            errorMessage = string.Empty;
+
+            using (var keyGenerator = new Rfc2898DeriveBytes(keyMaterial, IV, KeyDeriveIterations))
+            using (var aes = Aes.Create())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.IV = IV;
+                aes.Key = keyGenerator.GetBytes(aes.KeySize / BitsPerByte);
+                using (var cryptoStream = new CryptoStream(encryptedStream, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    cryptoStream.CopyTo(decryptedStream);
+                    decryptedStream.Flush();
+                }
+            }
+
+            return (int)SignDataResult.Success;
+        }
+
+        /// <summary>
+        /// Метод перешифрования данных.
+        /// </summary>
+        /// <param name="inputStream">Поток шифрованных данных (исходные данные).</param>
+        /// <param name="outputStream">Поток перешифрованных данных (результат).</param> 
+        /// <param name="encryptOptions">Параметры шифрования переданные с сервера.</param>
+        /// <param name="errorMessage">Сообщение об ошибке (если она есть).</param>
+        /// <returns>Результат выполнения операции (см. <see cref="SignDataResult"/>).</returns>
+        public static int ReEncrypt(Stream inputStream, Stream outputStream, string encryptOptions, out string errorMessage)
+        {
+            Logger.Debug($"ReEncrypt decryptOptions: {encryptOptions}");
+            errorMessage = string.Empty;
+
+            using (var ms = new MemoryStream())
+            {
+                using (var keyGenerator = new Rfc2898DeriveBytes(keyMaterial, IV, KeyDeriveIterations))
+                using (var aes = Aes.Create())
+                {
+                    aes.Mode = CipherMode.CBC;
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.IV = IV;
+                    aes.Key = keyGenerator.GetBytes(aes.KeySize / BitsPerByte);
+                    using (var cryptoStream = new CryptoStream(inputStream, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        cryptoStream.CopyTo(ms);
+                        ms.Flush();
+                    }
+                }
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                using (var keyGenerator = new Rfc2898DeriveBytes(newKeyMaterial, IV, KeyDeriveIterations))
+                using (var aes = Aes.Create())
+                {
+                    aes.Mode = CipherMode.CBC;
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.IV = IV;
+                    aes.Key = keyGenerator.GetBytes(aes.KeySize / BitsPerByte);
+                    using (var cryptoStream = new CryptoStream(outputStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        ms.CopyTo(cryptoStream);
+                        cryptoStream.FlushFinalBlock();
+                    }
+                }
+            }
+ 
+            return (int)SignDataResult.Success;
+        }
+    }
+}
---
+++ src/ClientCryptographyPlugin/Engine/EncryptorWrapper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace Library
+{
+    /// <summary>
+    /// Обертка над классом клиентского шифрования.
+    /// </summary>
+    /// <remarks>
+    /// Этот класс - часть API, нельзя его модифицировать.
+    /// </remarks>
+    public static class EncryptorWrapper
+    {
+        public static int Encrypt(string decryptedFilePath, string encryptedFilePath, string encryptionOptions, out string errorMessage)
+        {
+            try
+            {
+                using (var inStream = new FileStream(decryptedFilePath, FileMode.Open))
+                using (var outStream = new FileStream(encryptedFilePath, FileMode.Create))
+                {
+                    return Encryptor.Encrypt(inStream, outStream, encryptionOptions, out errorMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+                return (int)SignDataResult.UnknownError;
+            }
+        }
+
+        public static int Decrypt(string encryptedFilePath, string decryptedFilePath, string decryptionOptions, out string errorMessage)
+        {
+            try
+            {
+                using (var inStream = new FileStream(encryptedFilePath, FileMode.Open))
+                using (var outStream = new FileStream(decryptedFilePath, FileMode.Create))
+                {
+                    return Encryptor.Decrypt(inStream, outStream, decryptionOptions, out errorMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+                return (int)SignDataResult.UnknownError;
+            }
+        }
+
+        public static int ReEncrypt(string inputFilePath, string outputFilePath, string encryptionOptions, out string errorMessage)
+        {
+            try
+            {
+                using (var inStream = new FileStream(inputFilePath, FileMode.Open))
+                using (var outStream = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    return Encryptor.ReEncrypt(inStream, outStream, encryptionOptions, out errorMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+                return (int)SignDataResult.UnknownError;
+            }
+        }
+    }
+}
